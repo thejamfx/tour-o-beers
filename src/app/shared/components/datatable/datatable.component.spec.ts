07f38146623475cf41ee4b44b037d60646ad4bea
Simon Ly <kakasci@gmail.com>
2018-06-08 07:54:10 +0900

    Fix some unit tests

--- src/app/shared/components/datatable/datatable.component.spec.ts
+++ src/app/shared/components/datatable/datatable.component.spec.ts
@@ -3,23 +3,23 @@
 // import { DatatableComponent } from './datatable.component';
 
 // describe('DatatableComponent', () => {
-//   let component: DatatableComponent;
-//   let fixture: ComponentFixture<DatatableComponent>;
+//     let component: DatatableComponent;
+//     let fixture: ComponentFixture<DatatableComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ DatatableComponent ]
-//     })
-//     .compileComponents();
-//   }));
+//     beforeEach(async(() => {
+//         TestBed.configureTestingModule({
+//           declarations: [ DatatableComponent ]
+//         })
+//         .compileComponents();
+//     }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(DatatableComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+//     beforeEach(() => {
+//         fixture = TestBed.createComponent(DatatableComponent);
+//         component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+//     it('should create', () => {
+//         expect(component).toBeTruthy();
+//     });
 // });
--- src/app/shared/services/notificationService.service.spec.ts
+++ src/app/shared/services/notificationService.service.spec.ts
@@ -1,15 +1,17 @@
-// import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
-// import { NotificationService } from './notificationService.service';
+import { NotificationService } from './notificationService.service';
+import { MatSnackBarModule } from '@angular/material';
 
-// describe('NotificationService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [NotificationService]
-//     });
-//   });
+describe('NotificationService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [NotificationService],
+            imports: [MatSnackBarModule]
+        });
+    });
 
-//   it('should be created', inject([NotificationService], (service: NotificationService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+    it('should be created', inject([NotificationService], (service: NotificationService) => {
+        expect(service).toBeTruthy();
+    }));
+});
